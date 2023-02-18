9820064c6c238bbf1fce941a88892d2ed37ab579
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:49:27 +0200

    Add files via upload

---
+++ Prime Project 1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Prime_Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to start counting from: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number to stop counting to: ");
+            int fnum = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int c = num; c <= fnum; c++)
+            {
+                for (int i = 1; i <=c; i++)
+                {
+                    if (c % i == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 2)
+                {
+                    Console.WriteLine(c);
+                    counter = 0;
+                }
+                else
+                {
+                    counter = 0;
+                    continue;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prime or Odd project/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Prime_or_Odd_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to start counting from: ");
+            int num1=int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the number to stop counting to: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int tmp = 0;
+            for(int c=num1; c<=num2; c++) {
+               for(int i=1; i<=c; i++)
+                {
+                    if(c%i==0) { 
+                    tmp++;
+                    }
+                }
+                if (tmp == 2)
+                {
+                    Console.WriteLine(c);
+                    tmp= 0;
+                }
+                else
+                {
+                    tmp = 0;
+                    continue;
+                }
+            }
+            
+        }
+    }
+}
