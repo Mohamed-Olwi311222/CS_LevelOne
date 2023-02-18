2795f7f64263a880b85bd267ef4256ecade896a4
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-19 00:02:19 +0200

    Add files via upload

---
+++ problem 4(while loop)/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace problem_4_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("the number of subjects: ");
+            int number = int.Parse(Console.ReadLine());
+            int total = 0;
+            int i = 1;
+            while(i<=number){
+                Console.Write("the degree of subject {0}: ", i);
+                int degree = int.Parse(Console.ReadLine());
+                total = total + degree;
+                i++;
+
+            }
+            Console.Write("the total of your subjects degrees is {0} \n", total);
+            double average = (double)total / number;
+            Console.Write("your average is {0}", average);
+        }
+    }
+}
---
+++ problem 5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace problem_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("whats the value of x?") ;
+
+            
+            int x = int.Parse(Console.ReadLine());
+
+            
+            Console.Write("whats the value of y?");
+            
+            
+            int y = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("result={0}",x+y);
+        
+        }
+    }
+}
