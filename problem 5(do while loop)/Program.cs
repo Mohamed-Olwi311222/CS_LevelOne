32abfff8558457ffb4aceb671f3bc2155863e045
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-19 00:04:35 +0200

    Add files via upload

---
+++ problem 5(do while loop)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace problem_5_do_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do{
+                
+                int x = 0;
+                
+                do{
+                   Console.Write("*");
+                    x++;
+                } while (x <= i);
+                Console.WriteLine();
+                i++;
+            } while (i <= 4);
+        }
+    }
+}
---
+++ problem 5(while loop)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace problem_5_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 4)
+            {
+                int x = 0;
+                while (x <= i)
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                Console.WriteLine();
+                i++;
+            }
+        }
+    }
+}
