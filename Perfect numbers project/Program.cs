ecaf23d27d8d2db84b25afc23fd8b5336f078e32
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:48:20 +0200

    Add files via upload

---
+++ Perfect numbers project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Perfect_numbers_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to start counting from: ");
+            int num1=int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the number to stop counting to: ");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num1 < 1)
+            {
+                num1 = 1;   //if the user enterd negative or zero ,shift num1 to 1 instead
+            }
+            
+            for (int c=num1;c<=num2;c++)
+            {
+                int sum = 0;
+                for (int d=1;d<c;d++)
+                {
+                    if (c % d == 0)
+                       sum += d; 
+                    else
+                        continue;
+                }
+                if (sum == c)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ prime numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, c, tmp = 0; //tmp is for the counter of the numbers which num is divisible by
+
+            for (num = 2; num <= 100; num++)
+            {
+                for (c = 1; c <= num; c++)
+                {
+                    if (num % c == 0)  
+                    {
+                        tmp++; //increament tmp if num divisible by any numbers from 1 to itself
+                    }
+                }
+                if (tmp == 2) //to check if num divisible by 1 and itself only
+                {
+                    Console.WriteLine("{0} is prime", num);
+                    tmp = 0; //so it can come back to the if statement to check again
+                }
+                else //if tmp>2 then num is divisible by another number/s beside 1 and itself
+                {
+                    Console.WriteLine("{0} isnt prime ", num);
+                    tmp = 0; //so it can come back to the if statement to check again
+                }
+
+            }
+        }
+    }
+}
