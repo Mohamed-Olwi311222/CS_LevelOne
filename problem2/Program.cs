17478c1726c549e515b1a99bf2cdbd451471ab87
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-19 00:06:11 +0200

    Add files via upload

---
+++ problem2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("*****");
+
+            Console.WriteLine("****");
+
+            Console.WriteLine("***");
+
+            Console.WriteLine("**");
+
+            Console.WriteLine("*");
+
+        }
+    }   
+}
---
+++ problem3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace problem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare x to be an integar which equal 10
+            int x = 10;
+
+            //declare y to be an integar which equal 20
+            int y = 20;
+
+            //output the sum of x and y
+            Console.WriteLine("result={0}",x+y);
+        }
+    }
+}
