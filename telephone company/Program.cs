3dd270aa24e7966b023e5ec2f85520bfe38f0251
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-19 00:07:56 +0200

    Add files via upload

---
+++ telephone company/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace telephone_company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter your balance:");
+            float balance=float.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your call duration:");
+            float duration=float.Parse(Console.ReadLine());
+
+            float requiredbalance =(duration* 0.14f);
+
+            if (requiredbalance <= balance)
+            {
+                Console.WriteLine("Call will proceed");
+            }
+            else 
+            {
+                Console.WriteLine("insuffiecient balance,please recharge");
+            }
+        }
+    }
+}
