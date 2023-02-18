f4e657c3bf5fe27fff2f09b71a7064043d0adca6
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:57:32 +0200

    Add files via upload

---
+++ problem 2(do while loop)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace problem_2_do_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number: ");
+            int num = int.Parse(Console.ReadLine());
+            int i = 0;
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0} is even", i);
+
+                }
+                else
+                {
+                    Console.WriteLine("{0} is odd", i);
+                }
+                i++;
+            } while (i <= num);
+        }
+    }
+}
---
+++ problem 2(for loop)/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace problem_2_for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number: ");
+            int num=int.Parse(Console.ReadLine());
+            for(int i=0; i<=num; i++) {
+                if (i% 2 == 0)
+                {
+                    Console.WriteLine("{0} is even", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is odd", i);
+                }
+}
+        }
+    }
+}
