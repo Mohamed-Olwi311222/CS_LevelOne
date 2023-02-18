fb8e3ef84013abf255abc88853867411d73062d9
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-19 00:03:24 +0200

    Add files via upload

---
+++ problem 5 (for loop)/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace problem_5__for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<=4; i++)
+            {
+                for(int x = 0; x <= i; x++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ problem 5 sec 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace problem_5_sec_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("what is the value of the radius? ");
+            double r=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("the area of the circle={0} ",3.141 * r*r);
+
+
+
+
+        }
+    }
+}
