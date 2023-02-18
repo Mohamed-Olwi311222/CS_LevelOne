8137b547c1184f120923e9fd5fa4cf6cee8aba23
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:52:16 +0200

    Add files via upload

---
+++ problem 0 sec7(for loop)/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace problem_0_sec7_for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("putting kanka");
+                Console.WriteLine("putting coffee in a cup");
+                Console.Write("how many sugar spoons do u want? ");
+                int sugar = int.Parse(Console.ReadLine());
+
+                if (sugar > 0)
+                {
+                    for (int i = 1; i <= sugar; i++)
+                    {
+                        Console.WriteLine("putting sugar spoon number " + i);
+                    }
+                    Console.WriteLine("coffee is ready to serve");
+                }
+                else
+                {
+                    Console.WriteLine("coffee is ready to serve");
+                }
+
+            
+
+        
+    
+        }
+    }
+}
