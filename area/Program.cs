6695935ba990bbe23c1a909159499b293a72334b
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 21:17:51 +0200

    Add files via upload

---
+++ area/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("input radius");
+            int r=int.Parse(Console.ReadLine());
+            Console.WriteLine("area={0}",3.14*r*r);
+
+
+        }
+    }
+}
---
+++ balance problem(if)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.NetworkInformation;
+
+namespace balance_problem_if_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            float balance, duration, price;
+            Console.Write("Please enter your balance: ");
+            balance =float.Parse(Console.ReadLine());
+            
+            
+            Console.Write("Please enter your call duration: ");
+            duration = float.Parse(Console.ReadLine());
+
+
+            Console.Write("Please enter the price of the minute: ");
+            price = float.Parse(Console.ReadLine());
+
+
+            float requiredbalance=duration*price;
+
+            Console.Write("Is the user a spam? (true for yes and false for no) ");
+            bool spam=bool.Parse(Console.ReadLine());
+
+            if (requiredbalance <= balance && spam == false)
+            {
+                Console.WriteLine("Call will proceed");
+            }
+            else if(spam==true)
+            {
+                Console.WriteLine("User isnt allowed to call");
+            }
+            else if(requiredbalance > balance)
+            {
+                    Console.WriteLine("Insufficient balance,please recharge");
+            }
+
+
+
+            
+        }
+    }
+}
