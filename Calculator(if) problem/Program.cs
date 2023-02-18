4064bb64c44e404a870b1b89c95e688217a37d06
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:41:31 +0200

    Add files via upload

---
+++ Calculator(if) problem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.Common;
+
+namespace Calculator_if__problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float firstnum, secondnum;
+            char op;
+
+
+
+            Console.Write("First Number: ");
+            firstnum=float.Parse(Console.ReadLine());
+
+
+            Console.Write("Second Number: ");
+            secondnum = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the operator(' + '  for addition , ' - ' for subtraction, ' * ' for multiplication , ' / ' for division): ");
+           
+            op = char.Parse(Console.ReadLine());
+
+            while (op != '+' && op != '-' && op != '*' && op != '/')
+            {
+                Console.WriteLine("Please enter a correct operator: ");
+                op = char.Parse(Console.ReadLine());
+            }
+
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine("the addition is: {0}", firstnum + secondnum);
+                    break;
+                case '-':
+                    Console.WriteLine("the subtraction is: {0}", firstnum - secondnum);
+                    break;
+                case '*':
+                    Console.WriteLine("the multiplication is: {0}", firstnum * secondnum);
+                    break;
+                case '/':
+                    Console.WriteLine("the division is: {0}", firstnum / secondnum);
+                    break;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace calculator
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of X: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of Y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.Write("what is the operator that you need? (1 for division), (2 for multiplication) , (3 for substraction) ,(4 for addition) ");
+            int op = int.Parse(Console.ReadLine());
+
+            while (op > 4 || op <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("please choose the correct number for the operator");
+                op = int.Parse(Console.ReadLine());
+            }
+            
+            if (op == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("x/y= ");
+                Console.Write(x / y);
+
+            }
+            else if (op == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("x*y= ");
+                Console.Write(x * y);
+
+            }
+            else if (op == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("x-y= ");
+                Console.Write(x - y);
+
+            }
+            else if (op == 4)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("x+y= ");
+                Console.Write(x + y);
+
+            }
+
+            Thread.Sleep(10000);
+
+        }
+    }
+}
