4caad68fbf9262879ac2376c5f55276e67a8b7b8
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 21:20:16 +0200

    Add files via upload

---
+++ bitwise/Program.cs
@@ -0,0 +1,42 @@
+﻿ namespace bitwise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b ,result ;
+            a = 48;
+            b = 21;
+            
+            Console.Write("a's complement: ");
+            Console.WriteLine(~a);
+            
+            Console.Write("b's complement: ");
+            Console.WriteLine(~b);
+            
+            Console.Write("a and b: ");
+            Console.WriteLine(a&b);
+            
+            result=a | b ;
+            Console.WriteLine("result OR="+result);
+            
+            result = a ^ b;
+            Console.WriteLine("result XOR:"+result);
+
+            result = a << 2;
+            Console.WriteLine("left shift of a:" + result);
+
+            result = a >> 3;
+            Console.WriteLine("right shift of a:" + result);
+
+            
+            result = b << 2;
+            Console.WriteLine("left shift of b:" + result);
+
+            result = b >> 3;
+            Console.WriteLine("right shift of b:" + result);
+
+
+        }
+    }
+}
---
+++ bulb problem (if)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace bulb_problem__if_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool close=true;
+
+            if (close=true)
+            {
+                Console.WriteLine("opening the bulb");
+            }else if (close != true)
+            {
+                Console.WriteLine("bulb is already open");
+            }
+            else
+            {
+                Console.WriteLine("bulb is broken , please repair");
+            }
+
+
+
+
+        }
+    }
+}
