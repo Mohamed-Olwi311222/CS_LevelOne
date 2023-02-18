3356259791e3af0a609cf8ba5104ab6389e476d4
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:47:15 +0200

    Add files via upload

---
+++ if statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool closed= false ;
+            bool broken = true;
+
+            if (closed)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("lamp is opening ");
+            }
+            else if(broken)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("the lamp is broken, please repair");
+                
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("lamp is already opened");
+            }
+        }
+    }
+}
---
+++ multiplication table(for loop)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace multiplication_table_for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table");
+            for(int i=1; i<=9; i++) {
+                for(int x=1; x<=9; x++) {
+
+                    Console.WriteLine("{0}*{1}={2} ",i,x,i*x);
+                
+                }    
+            
+            }
+        }
+    }
+}
