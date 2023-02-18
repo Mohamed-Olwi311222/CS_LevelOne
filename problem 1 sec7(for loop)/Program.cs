3709fc8336cce495ec5027fd8351f1131558a6af
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:54:42 +0200

    Add files via upload

---
+++ problem 1 sec7(for loop)/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace problem_1_sec7_for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number which do you want to keep adding to: ");
+            int num=int.Parse(Console.ReadLine());
+            int sum=0;
+            for(int i=0; i<=num; i++)
+            {
+                sum =i+sum;
+            }
+            Console.WriteLine("the submission is:{0}", sum);
+
+
+
+        }
+    }
+}
---
+++ problem 1(do while loop)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace problem_1_do_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number which do you want to keep adding to: ");
+            int num = int.Parse(Console.ReadLine());
+
+
+            int sum = 0, i = 0;
+            do
+            {
+
+                sum = i + sum;
+                i++;
+            } while (i <= num);
+                Console.WriteLine("the submission is:{0}", sum);
+
+
+
+        }
+    }
+}
