8c348341071cbd6d301b785dcf8ed281e2e467b9
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:55:59 +0200

    Add files via upload

---
+++ problem 1(while loop)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace problem_1_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number which do you want to keep adding to: ");
+            int num = int.Parse(Console.ReadLine());
+
+
+            int sum=0, i = 0;
+            while (i <= num)
+            {
+
+               sum=i+sum ;
+                i++;
+            }
+            Console.WriteLine("the submission is:{0}", sum);
+
+
+
+        }
+    }
+}
---
+++ problem 2 sec 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number: ");
+            int num=int.Parse(Console.ReadLine());
+
+            for(int x=0; x<=num; x++)
+            {
+                if(x%2==0)
+                {
+                    Console.WriteLine("{0} is even", x);
+                }else                 {
+                    Console.WriteLine("{0} is odd", x);
+                }
+
+            }
+
+        }
+    }
+}
