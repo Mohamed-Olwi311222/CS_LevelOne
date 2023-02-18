985a6ae296f2d0880a3aeb5cbb289e42610e17b7
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:50:58 +0200

    Add files via upload

---
+++ problem 0 sec 7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace coffeproblemv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("putting kanka");
+            Console.WriteLine("putting coffe in a cup");
+            Console.Write("how many sugar spoons do u want? ");
+            int sugar = int.Parse(Console.ReadLine());
+            
+            if (sugar > 0)
+            {
+                for (int i = 1; i <= sugar; i++)
+                {
+                    Console.WriteLine("putting sugar spoon number " + i);
+                }
+                Console.WriteLine("coffe is ready to serve");
+            }else{
+                Console.WriteLine("coffe is ready to serve");
+            }
+ 
+        }
+
+    }
+}
---
+++ problem 0 sec7(do while loop)/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace problem_0_sec7_do_while_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("putting kanka");
+            Console.WriteLine("putting coffee in a cup");
+            Console.Write("how many sugar spoons do u want? ");
+            int sugar = int.Parse(Console.ReadLine());
+
+            if (sugar > 0)
+            {
+                int i = 1;
+                do
+                {
+                    Console.WriteLine("putting sugar spoon number " + i);
+                    i++;
+                } while (i <= sugar);
+                Console.WriteLine("coffee is ready to serve");
+            }
+            else
+            {
+                Console.WriteLine("coffee is ready to serve");
+            }
+
+        }
+    }
+}
