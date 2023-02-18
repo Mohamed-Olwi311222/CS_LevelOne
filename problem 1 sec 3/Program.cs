9bd11b2473ef5f7f631f7c579221a25892ccda59
AceMido <111067383+AceMido@users.noreply.github.com>
2023-02-18 23:53:37 +0200

    Add files via upload

---
+++ problem 1 sec 3/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace problem_1_sec_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             Console.Write("whats the value of x? ");
+             float x = float.Parse(Console.ReadLine());
+
+            Console.Write("whats the value of y? ");
+            float y= float.Parse(Console.ReadLine());
+
+            Console.Write("which operand do you want? (1 for addition) (2 for substraction) (3 for multiplication) (4 for division) ");
+            int op=int.Parse(Console.ReadLine());
+
+
+
+
+
+            while (op <= 0 || op > 4)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("please enter the correct value for each opearand ");
+                op = int.Parse(Console.ReadLine());
+            }
+                
+            
+
+            if (op == 1)
+            {
+                Console.Write("the value of the sum is: {0}",x+y);
+                
+            }else if(op == 2)
+            {
+                Console.Write("the value of the substraction is: {0}",x-y);
+            }else if (op == 3)
+            {
+                Console.Write("the value of the multiplication is: {0}",x*y);
+            }else if(op==4)
+            {
+                Console.Write("the value of the division is: {0}",x/y);
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ problem 1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace problem1
+{
+    class problem1
+    {
+        static void Main(string[] args)
+        {
+            //declar a var to start, condtion which each time the loop check , what to do to the var after every loop//
+            for(int x=0; x < 2; x++)
+            {
+                Console.WriteLine("***");
+            }
+        }
+    }
+}
